8b415e851482e0c0c29308f89fbf02b7559a2631
Asaduzzaman14 <asadasad9282@gmail.com>
2024-06-10 23:11:24 +0600

    profile update

--- src/pages/Profile/BasicDetails.tsx
+++ src/pages/Profile/BasicDetails.tsx
@@ -1,23 +1,26 @@
-import { useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { getTopUpToken } from '../../hooks/handelAdminToken';
+import { useState } from 'react';
+import Swal from 'sweetalert2';
 
 interface UserProfile {
   id: string;
   name: string;
-  email: string;
   phone: string;
-  password: string;
-  referralCode: string;
-  myReferralCode: string;
-  role: string;
-  profileImage: string | null;
-  referralCount: number;
-  nativeWallet: number;
+  email: string;
   createdAt: string;
   updatedAt: string;
 }
 
-const BasicDetails = ({ profile }: any) => {
+const BasicDetails = ({ profile, fetchData }: any) => {
   console.log(profile);
+
+  const token = getTopUpToken();
+
+  const [updateLodaing, setUpdateLodaing] = useState(false);
+
+  const [bonusData, setBonusData] = useState<any>([]);
+
   const {
     register,
     handleSubmit,
@@ -25,9 +28,52 @@ const BasicDetails = ({ profile }: any) => {
     formState: { errors },
   } = useForm<UserProfile>();
 
+  const onSubmit: SubmitHandler<UserProfile> = async (data: UserProfile) => {
+    setUpdateLodaing(true);
+
+    const newData = { ...data, id: bonusData[0]?.id };
+    try {
+      const response = await fetch(
+        `http://localhost:5000/api/v1/user/${profile?._id}`,
+        {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `${token}`,
+          },
+          body: JSON.stringify(newData),
+        },
+      );
+      fetchData();
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const responseData = await response.json();
+      setUpdateLodaing(false);
+      if (responseData.success) {
+        reset();
+
+        Swal.fire({
+          title: 'success',
+          text: 'Successfully Updateed',
+          icon: 'success',
+        });
+      }
+    } catch (error) {
+      Swal.fire({
+        title: 'Faield',
+        text: 'Failed to update settings',
+        icon: 'error',
+      });
+    }
+  };
+
   return (
     <>
-      <form className="flex flex-col gap-3 px-4 lg:w-[500px]">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-3 px-4 lg:w-[500px]"
+      >
         <div className="w-full">
           <label className="mb-2.5 block text-black dark:text-white">
             Name
@@ -35,7 +81,7 @@ const BasicDetails = ({ profile }: any) => {
           <input
             type="text"
             placeholder="Enter your last name"
-            {...register('name')}
+            {...register('name', { required: 'This field is required' })}
             defaultValue={profile?.name as string}
             className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
           />
@@ -46,6 +92,7 @@ const BasicDetails = ({ profile }: any) => {
             Email
           </label>
           <input
+            disabled
             type="email"
             {...register('email')}
             defaultValue={profile?.email as string}
