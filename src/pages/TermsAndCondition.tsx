9e40eb07506ff9a1937136a657af3a7cc14718bc
Asaduzzaman14 <asadasad9282@gmail.com>
2024-06-23 22:14:59 +0600

    Terms-and-condition

--- src/pages/TermsAndCondition.tsx
+++ src/pages/TermsAndCondition.tsx
@@ -1,9 +1,102 @@
-import React from 'react';
-
 const TermsAndCondition = () => {
   return (
     <div>
-      <h2>terms-and-condition</h2>
+      <div className="px-4 lg:px-10 md:pr-10 text-black mb-3">
+        <h2 className="text-xl font-semibold mt-3">Terms-and-condition</h2>
+        <div>
+          <p className="mt-2  font-normal">
+            Welcome to Aj gameshop. Please read these Terms and Conditions
+            ("Terms") carefully before using our game credits selling app. By
+            downloading, accessing, or using our app, you agree to be bound by
+            these Terms. If you do not agree to these Terms, do not use our app.
+          </p>
+          <h2 className="text-xl font-semibold mt-3">1. Acceptance of Terms</h2>
+          <p className="mt-2  font-norma">
+            By using Aj Gameshop, you confirm that you have read, understood,
+            and agree to these Terms. These Terms may be updated from time to
+            time, and continued use of the app constitutes acceptance of the new
+            Terms.
+          </p>
+          <h2 className="text-xl font-semibold mt-3">2. Eligibility</h2>
+          <p className="mt-2  font-norma">
+            You must be at least 18 years old to use Aj gameshop. By using the
+            app, you represent and warrant that you meet this age requirement.
+          </p>
+          <h2 className="text-xl font-semibold mt-3">
+            3. Account Registration
+          </h2>
+          <p className="mt-2  font-normal">
+            To use certain features of the app, you must create an account. You
+            are responsible for maintaining the confidentiality of your account
+            information and for all activities that occur under your account.
+          </p>
+          <h2 className="text-xl font-semibold mt-3">
+            4. Purchase and Sale of Credits
+          </h2>
+          <p className="mt-2  font-normal">
+            You can purchase game credits through the app using the available
+            payment methods. All sales are final, and no refunds will be
+            provided unless required by law.
+          </p>
+          <h2 className="text-xl font-semibold mt-3">
+            5. Pricing and Payments
+          </h2>
+          <p className="mt-2  font-normal">
+            Prices for game credits are set by sellers and may vary. Payments
+            are processed through third-party payment providers, and you agree
+            to their terms and conditions as well.
+          </p>
+          <h2 className="text-xl font-semibold mt-3">6. User Conduct </h2>
+          You agree not to engage in any activity hat: - Violates any laws or
+          regulations - Infringes on the rights of others - Is fraudulent,
+          misleading, or deceptive - Interferes with the operation of the app
+          <h2 className="text-xl font-semibold mt-3">
+            7. Intellectual Property
+          </h2>
+          <p className="mt-2  font-normal">
+            All content, trademarks, and data on the app, including but not
+            limited to software, text, graphics, logos, and images, are the
+            property of [App Name] or its licensors and are protected by
+            intellectual property laws.
+          </p>
+          <h2 className="text-xl font-semibold mt-3">8. Privacy </h2>
+          <p className="mt-2  font-norma">
+            Our Privacy Policy describes how we collect, use, and disclose your
+            personal information. By using the app, you agree to our Privacy
+            Policy.
+          </p>
+          <h2 className="text-xl font-semibold mt-3">
+            9. Limitation of Liability
+          </h2>
+          <p className="mt-2  font-normal">
+            Aj gameshop is not liable for any direct, indirect, incidental,
+            special, or consequential damages arising from your use of the app,
+            including but not limited to any errors or omissions in content,
+            loss of data, or any unauthorized access to your account.
+          </p>
+          <h2 className="text-xl font-semibold mt-3">10. Termination</h2>
+          <p className="mt-2  font-norma">
+            We may terminate or suspend your account and access to the app at
+            any time, without prior notice or liability, for any reason
+            whatsoever, including if you breach these Terms.
+          </p>
+          <h2 className="text-xl font-semibold mt-3">11. Governing Law</h2>
+          <p className="mt-2  font-norma">
+            These Terms are governed by and construed in accordance with the
+            laws of Bangladesh. without regard to its conflict of law
+            principles.
+          </p>
+          <h2 className="text-xl font-semibold mt-3">12. Contact Us</h2>
+          <p className="mt-2  font-norma">
+            If you have any questions about these Terms, please contact us at
+            <span className="font-semibold ">contact.ajgameshop@gmail.com</span>
+          </p>
+          <p className="mt-2  font-normal">
+            By using Aj gameshop, you agree to these Terms and Conditions. Thank
+            you for choosing our app!
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
